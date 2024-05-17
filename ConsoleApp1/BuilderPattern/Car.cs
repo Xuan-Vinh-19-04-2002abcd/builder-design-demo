c0b0b0fd744c8418cbc2fd764eb3e4235ae2a5d9
xuand <xuandangvinh@gmail.com>
2024-05-17 21:15:18 +0700

    abc

---
+++ ConsoleApp1/BuilderPattern/Car.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp1.BuilderPattern;
+
+public class Car
+{
+    public int NumberOfWheels { get; set; }
+    public string Color { get; set; }
+    public string NameCar { get; set; }
+
+    public Car(int numberOfWheels, string color, string nameCar)
+    {
+        NumberOfWheels = numberOfWheels;
+        Color = color;
+        NameCar = nameCar;
+    }
+    public override string ToString()
+    {
+        var content = "";
+        content += $"Number of wheels:    \t {NumberOfWheels}\n";
+        content += $"Color:               \t {Color}\n";
+        content += $"Name:               \t {NameCar}\n";
+        return content;
+    }   
+}
---
+++ ConsoleApp1/BuilderPattern/CarBuilder.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp1.BuilderPattern;
+
+public class CarBuilder : ICarBuilder
+{
+    public int NumberOfWheels { get; set; }
+    public string Color { get; set; }
+    public string NameCar { get; set; }
+    public CarBuilder AddWeels(int numberOfWheels)
+    {
+        NumberOfWheels = numberOfWheels;
+        return this;
+    }
+    public CarBuilder Paint(string color)
+    {
+        Color = color;
+        return this;
+    }
+    public CarBuilder AddNameCar(string nameCar)
+    {
+        NameCar = nameCar;
+        return this;
+    }
+    public Car Build()
+    {   
+        return new Car(NumberOfWheels,Color,NameCar);
+    }
+}
